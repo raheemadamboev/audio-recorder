9547e4b50368163febc67cab2d8bd7b9fd54ba86
raheemadamboev <vividricemaryandmax@gmail.com>
2023-03-04 00:09:08 +0500

    Built UI;

--- app/src/main/java/xyz/teamgravity/audiorecorder/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/audiorecorder/MainActivity.kt
@@ -1,27 +1,73 @@
 package xyz.teamgravity.audiorecorder
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import xyz.teamgravity.audiorecorder.ui.theme.AudioRecorderTheme
+import java.io.File
 
 class MainActivity : ComponentActivity() {
+
+    private val recorder: AudioRecorder by lazy { AudioRecorder(applicationContext) }
+    private val player: AudioPlayer by lazy { AudioPlayer(applicationContext) }
+
+    private var audio: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestPermissions(arrayOf(Manifest.permission.RECORD_AUDIO), 0)
         setContent {
             AudioRecorderTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Button(
+                            onClick = {
+                                audio = File(cacheDir, "audio.mp3")
+                                recorder.start(audio ?: return@Button)
+                            }
+                        ) {
+                            Text(text = stringResource(id = R.string.start_recording))
+                        }
+                        Button(
+                            onClick = {
+                                recorder.stop()
+                            }
+                        ) {
+                            Text(text = stringResource(id = R.string.stop_recording))
+                        }
+                        Button(
+                            onClick = {
+                                player.play(audio ?: return@Button)
+                            }
+                        ) {
+                            Text(text = stringResource(id = R.string.play))
+                        }
+                        Button(
+                            onClick = {
+                                player.stop()
+                            }
+                        ) {
+                            Text(text = stringResource(id = R.string.stop))
+                        }
+                    }
                 }
             }
         }
